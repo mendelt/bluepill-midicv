6f0cd8a244925137571d2c8614c588c81a19457f
Mendelt Siebenga <mendelt@mendelt.nl>
2023-01-15 22:09:44 +0100

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,16 @@
 
 extern crate panic_semihosting;
 
-use cortex_m_rt::{entry};
+use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use hal::prelude::*;
 use hal::stm32;
-
 
 #[entry]
 fn main() -> ! {
     let _peripherals = cortex_m::Peripherals::take().unwrap();
     let _device = stm32::Peripherals::take().unwrap();
 
-    hprintln!("Hello semihosting world").unwrap();
+    hprintln!("Hello semihosting world");
 
-    loop { }
+    loop {}
 }
-
